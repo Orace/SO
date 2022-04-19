87b2a0a4059b1a0db72ac0612d5ded635c03eaa7
Orace <pierre.lando@gmail.com>
2022-04-19 10:24:44 +0200

    [SO_71919433] Initial push.

---
+++ SO_71919433/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Windows;
+using LibVLCSharp.Shared;
+
+namespace SO_71919433;
+
+public partial class MainWindow
+{
+    private readonly LibVLC _libVlc;
+    private readonly MediaPlayer _mediaPlayer;
+
+    public MainWindow()
+    {
+        _libVlc = new LibVLC();
+        _mediaPlayer = new MediaPlayer(_libVlc);
+        InitializeComponent();
+
+        VideoView.Loaded += VideoViewOnLoaded;
+    }
+
+    private void VideoViewOnLoaded(object sender, RoutedEventArgs e)
+    {
+        VideoView.MediaPlayer = _mediaPlayer;
+    }
+
+    private void PlayButtonOnClick(object sender, RoutedEventArgs e)
+    {
+        using var media = new Media(_libVlc, new Uri("http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"));
+        VideoView.MediaPlayer?.Play(media);
+    }
+}
