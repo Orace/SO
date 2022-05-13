708840303abd6370de28bd01cce061a2f10149fa
Orace <pierre.lando@gmail.com>
2022-05-13 18:13:38 +0200

    [SO_72231442] Use JSON Path

--- SO_72231442/Program.cs
+++ SO_72231442/Program.cs
@@ -1,29 +1,26 @@
-﻿    using Newtonsoft.Json;
-    using Newtonsoft.Json.Linq;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
-    // read the JSON file. 
-    var json = JObject.Parse(File.ReadAllText("input.json"));
+// read the JSON file. 
+var json = JObject.Parse(File.ReadAllText("input.json"));
 
-    // Extract label by GUID. Note that GUID case is inconsistent within the file, so we force upper case.
-    var labelByGuid = json["cases"]["procedure"]["version"]["forms"]
-                     .SelectMany(t => t["sections"])
-                     .SelectMany(t => t["rows"])
-                     .SelectMany(t => t["fields"])
-                     .ToDictionary(t => t["guid"].Value<string>().ToUpperInvariant(),
-                                   t => t["label"].Value<string>());
+// Extract label by GUID. Note that GUID case is inconsistent within the file, so we force upper case.
+var labelByGuid = json.SelectTokens("cases.procedure.version.forms[*].sections[*].rows[*].fields[?(@.guid && @.label)]")
+                      .ToDictionary(t => t["guid"]!.ToString().ToUpperInvariant(),
+                                    t => t["label"]!.ToString());
 
-    // Update entries.
-    foreach (var entry in json["cases"]["content"])
-    {
-        var field = entry["field"].Value<string>().ToUpperInvariant();
-        if (!labelByGuid.TryGetValue(field, out var label))
-            continue;
-        
-        entry["field"] = label;
-    }
+// Update entries.
+foreach (var entry in json.SelectTokens("cases.content.[?(@.field)]"))
+{
+    var field = entry["field"]!.ToString().ToUpperInvariant();
+    if (!labelByGuid.TryGetValue(field, out var label))
+        continue;
 
-    // write back the JSON object.
-    var settings = new JsonSerializerSettings { Formatting = Formatting.Indented };
-    var serializer = JsonSerializer.Create(settings);
-    await using var outputStream = File.CreateText(@"output.json");
-    serializer.Serialize(outputStream, json);
+    entry["field"] = label;
+}
+
+// write back the JSON object.
+var settings = new JsonSerializerSettings { Formatting = Formatting.Indented };
+var serializer = JsonSerializer.Create(settings);
+await using var outputStream = File.CreateText(@"output.json");
+serializer.Serialize(outputStream, json);
