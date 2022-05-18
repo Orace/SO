113d131a351d934dd72259cd3788007981abf1a6
Orace <pierre.lando@gmail.com>
2022-05-18 15:41:14 +0200

    [SO_72276645] Add support for one to many relation

--- SO_72276645/DbSetExtension.cs
+++ SO_72276645/DbSetExtension.cs
@@ -24,29 +24,71 @@ public static class DbSetExtension
         var entityType = originalSet.EntityType;
         var containsMethod = typeof(string).GetMethod("Contains", new[] { typeof(string) })!;
 
+        // Ack to retrieve Enumerable.Any<>(Func<,>)
+        var anyMethod = typeof(Enumerable)
+                       .GetMethods()
+                       .Single(m => m.Name == "Any" &&
+                                    m.GetParameters().Length == 2);
+
         // {x}
         var xParam = Expression.Parameter(typeof(T), "x");
 
         // {search}
         var constExpr = Expression.Constant(search);
 
-        // {x.Name} list
+        // {x.Name.Contains(search)} list
         var xDotNames = entityType.GetProperties()
                                   .Where(p => p.ClrType == typeof(string))
-                                  .Select(p => Expression.Property(xParam, p.Name));
+                                  .Select(p => Expression.Property(xParam, p.Name))
+                                  .Select(e => (Expression)Expression.Call(e, containsMethod, constExpr));
 
-        // {x.Navigation.Name} list
+        // {x.Navigation.Name.Contains(search)} list
         var xDotOtherDotNames = entityType.GetDeclaredNavigations()
                                           .Where(n => !n.IsCollection)
                                           .SelectMany(n => n.TargetEntityType
                                                             .GetProperties()
                                                             .Where(p => p.ClrType == typeof(string))
-                                                            .Select(p => NestedProperty(xParam, n.Name, p.Name)));
+                                                            .Select(p => NestedProperty(xParam, n.Name, p.Name)))
+                                          .Select(e => (Expression)Expression.Call(e, containsMethod, constExpr)); ;
 
-        // { || x.{}.Name.Contains(search) }
+        // {x.Navigations.Any(n => n.Name.Contains(search))} list
+        var xDotOthersDotNames = entityType.GetDeclaredNavigations()
+                                           .Where(n => n.IsCollection)
+                                           .SelectMany(n =>
+                                           {
+                                               var nType = n.ForeignKey.DeclaringEntityType.ClrType;
+
+                                               // Enumerable.Any<NType>
+                                               var genericAnyMethod = anyMethod.MakeGenericMethod(nType);
+
+                                               // {n}
+                                               var nParam = Expression.Parameter(nType, "n");
+
+                                                // {x.Navigations}
+                                                var xDotNavigations = Expression.Property(xParam, n.Name);
+
+                                                return n.TargetEntityType
+                                                        .GetProperties()
+                                                        .Where(p => p.ClrType == typeof(string))
+                                                        .Select(p =>
+                                                         {
+                                                             // {n.Name}
+                                                             var nDotName = Expression.Property(nParam, p.Name);
+                                                             
+                                                             // {n.Name.Contains(search)}
+                                                             var contains = Expression.Call(nDotName, containsMethod, constExpr);
+
+                                                             // {n => n.Name.Contains(search)}
+                                                             var lambda = Expression.Lambda(contains, nParam);
+
+                                                             return Expression.Call(null, genericAnyMethod, xDotNavigations, lambda);
+                                                         });
+                                            }).ToList();
+
+        // { || ... }
         var orExpression = xDotNames.Concat(xDotOtherDotNames)
-                                    .Select(e => (Expression)Expression.Call(e, containsMethod, constExpr))
-                                    .Aggregate(Expression.OrElse);
+                                    .Concat(xDotOthersDotNames)
+                                    .Aggregate(Expression.Or);
 
         var lambda = Expression.Lambda<Func<T, bool>>(orExpression, xParam);
 
