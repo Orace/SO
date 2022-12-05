d8dd560b2aa49bc7c174d6676fa0597beba1027f
Orace <pierre.lando@gmail.com>
2022-12-05 17:22:42 +0100

    [SO_74690756] Initial commit.

---
+++ SO_74690756/Program.cs
@@ -0,0 +1,67 @@
+﻿using Newtonsoft.Json;
+
+Try(@"{ ""BoolValue"": true }");
+Try(@"{ ""BoolValue"": false }");
+Try(@"{ ""BoolValue"": ""Y"" }");
+Try(@"{ ""BoolValue"": ""N"" }");
+Try(@"{ ""BoolValue"": 10 }");
+Try(@"{ ""BoolValue"": ""Z"" }");
+
+Console.ReadLine();
+
+void Try(string json)
+{
+    var model = JsonConvert.DeserializeObject<Model>(json);
+    var content = model is null
+                      ? "null model"
+                      : model.BoolValue switch
+                      {
+                          true => "true",
+                          false => "false",
+                          null => "null"
+                      };
+
+    Console.WriteLine(json);
+    Console.WriteLine($"Got a {content}");
+    Console.WriteLine();
+}
+
+public class Model
+{
+    [JsonConverter(typeof(YNBoolConverter))]
+    public bool? BoolValue { get; set; }
+}
+
+public class YNBoolConverter : JsonConverter
+{
+    public override void WriteJson(JsonWriter writer, object? value, JsonSerializer serializer)
+    {
+        writer.WriteValue(value switch
+        {
+            true => "Y",
+            false => "N",
+            _ => string.Empty
+        });
+    }
+
+    public override object? ReadJson(JsonReader reader, Type objectType, object? existingValue, JsonSerializer serializer)
+    {
+        return reader.Value switch
+        {
+            true => true,
+            "Y" => true,
+            "y" => true,
+
+            false => false,
+            "N" => false,
+            "n" => false,
+
+            _ => existingValue
+        };
+    }
+
+    public override bool CanConvert(Type objectType)
+    {
+        return objectType == typeof(string);
+    }
+}
