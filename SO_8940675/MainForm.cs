ef30285325b27d2119db85b6d7c1dff31eea9f84
Pierre Lando <p.lando@csinstruments.eu>
2024-06-27 17:04:31 +0200

    [SO_8940675] Remove useless binding to SelectedValue

--- SO_8940675/MainForm.cs
+++ SO_8940675/MainForm.cs
@@ -21,10 +21,8 @@ public partial class MainForm : Form
 
         comboBox1.DataBindings.Add(nameof(comboBox1.DataSource), model, nameof(model.Values1), true, DataSourceUpdateMode.Never);
         comboBox1.DataBindings.Add(nameof(comboBox1.SelectedItem), model, nameof(model.Value1), true, DataSourceUpdateMode.OnPropertyChanged);
-        comboBox1.DataBindings.Add(nameof(comboBox1.SelectedValue), model, nameof(model.Value1), true, DataSourceUpdateMode.OnPropertyChanged);
 
         comboBox2.DataBindings.Add(nameof(comboBox2.DataSource), model, nameof(model.Values2), true, DataSourceUpdateMode.Never);
         comboBox2.DataBindings.Add(nameof(comboBox2.SelectedItem), model, nameof(model.Value2), true, DataSourceUpdateMode.OnPropertyChanged);
-        comboBox2.DataBindings.Add(nameof(comboBox2.SelectedValue), model, nameof(model.Value2), true, DataSourceUpdateMode.OnPropertyChanged);
     }
 }
